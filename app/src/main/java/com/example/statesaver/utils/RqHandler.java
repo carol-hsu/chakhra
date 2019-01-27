fd40829b8ef48d16a875b46b1375e640a2c5c702
carol_hsu <carol_hsu@trend.com.tw>
2019-01-27 02:57:23 -0500

    conntect check

--- app/src/main/java/com/example/statesaver/utils/RqHandler.java
+++ app/src/main/java/com/example/statesaver/utils/RqHandler.java
@@ -3,16 +3,31 @@ package com.example.statesaver.utils;
 import com.example.statesaver.types.RequestItem;
 import java.util.List;
 import android.content.Context;
-
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 
 public class RqHandler extends Thread {
 
     private DbHandler dbHandler;
     private boolean is_run = false;
+    private ConnectivityManager connectManager;
 
     public RqHandler(Context context){
         is_run = true;
         dbHandler = DbHandler.getInstance(context);
+        connectManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+    }
+
+    private boolean hasInternet(){
+        boolean is_connect = true;
+        if(connectManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState()
+                != NetworkInfo.State.CONNECTED &&
+                connectManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState()
+                        != NetworkInfo.State.CONNECTED) {
+            //we are connected to a network
+            is_connect = false;
+        }
+        return is_connect;
     }
 
     public void run(){
@@ -23,7 +38,11 @@ public class RqHandler extends Thread {
                 List<RequestItem> requestList = dbHandler.getRequests();
                 if(!requestList.isEmpty()) {
                     System.out.println("it has: "+requestList.size()+" items");
+                    if(!hasInternet()){
+                        System.out.println("No connect");
+                    }
                 }
+
             } catch (Exception e) {
                 System.out.println("request handler: "+ e.toString());
             }
